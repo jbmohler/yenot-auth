cc187bacf33e58cf7597bcc293cbc4cf9c30a005
Joel B. Mohler <joel@kiwistrawberry.us>
2020-05-09 09:05:53 -0400

    report information endpoints

--- yenotauth/server/endpoints.py
+++ yenotauth/server/endpoints.py
@@ -1,6 +1,7 @@
 from bottle import request
 import rtlib
 import yenot.backend.api as api
+import yenotauth.core
 
 app = api.get_global_app()
 
@@ -50,4 +51,80 @@ def get_api_endpoints():
     results.keys["client-relateds"] = [
         ("Register Endpoints", "yenot:activities/register")
     ]
+    return results.json_out()
+
+
+class ReportMetaXformer:
+    def __init__(self, epname):
+        self.routes = {r.name: r for r in app.routes if r.name == epname}
+
+    def xform(self, oldrow, row):
+        row.url = self.routes[row.name].rule[1:]
+        row.prompts = yenotauth.core.route_prompts(self.routes[row.name])
+        row.sidebars = yenotauth.core.route_sidebars(self.routes[row.name])
+
+
+@app.get("/api/report/<name>/runmeta", name="api_report_runmeta")
+def api_report_runmeta(name):
+    select = """
+select 
+    activities.act_name, activities.description, 
+    activities.url, activities.note, activities.id
+from activities
+where name=%(n)s
+"""
+
+    results = api.Results()
+    with app.dbconn() as conn:
+        cm = api.ColumnMap(
+            url=api.cgen.basic(),
+            prompts=api.cgen.__meta__(),
+            id=api.cgen.yenot_report.surrogate(),
+        )
+
+        rawdata = api.sql_tab2(conn, select, {"n": name}, cm)
+
+        if len(rawdata[1]) == 0:
+            raise api.UserError(
+                "invalid-param", "The report could not be found by this name."
+            )
+
+        xform = ReportMetaXformer(name)
+        columns = api.tab2_columns_transform(
+            rawdata[0], insert=[("url", "prompts", "sidebars")]
+        )
+        rows = api.tab2_rows_transform(rawdata, columns, xform.xform)
+
+    results.tables["endpoint", True] = columns, rows
+    return results.json_out()
+
+
+@app.get("/api/report/<name>/info", name="api_report_info")
+def api_report_info(name):
+    select = """
+select autoid, name, description, url, note, technical
+from activities
+where name=%(n)s
+"""
+
+    results = api.Results()
+    with app.dbconn() as conn:
+        rawdata = api.sql_tab2(conn, select, {"n": name})
+
+        xform = ReportMetaXformer(name)
+        columns = api.tab2_columns_transform(
+            rawdata[0], insert=[("url", "method", "title", "prompts", "sidebars")]
+        )
+
+        def xform_squared(oldrow, row):
+            if name in xform.routes:
+                row.method = xform.routes[name].method
+            if "report_title" in xform.routes[name].config:
+                row.title = xform.routes[name].config["report_title"]
+
+            xform.xform(oldrow, row)
+
+        rows = api.tab2_rows_transform(rawdata, columns, xform_squared)
+
+        results.tables["endpoints", True] = columns, rows
     return results.json_out()
